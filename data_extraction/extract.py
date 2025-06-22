826e94b06c44470e927e28b4ddc90ca9950ebc3e
jerome <jeromechua.jz@gmail.com>
2025-06-22 22:25:37 +0800

    added data extraction file

---
+++ data_extraction/extract.py
@@ -0,0 +1,25 @@
+import requests
+from pathlib import Path
+import os
+
+
+def main():
+    try:
+        url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet"
+        print(f"extracting file from {url}...")
+        response = requests.get(url)
+        # get root directory
+        root_dir = Path(__file__).resolve().parents[1] 
+        # ensure data directory exists
+        os.makedirs(f"{root_dir}/data", exist_ok=True)
+        file_path = f"{root_dir}/data/yellow_tripdata_2024-01.parquet"
+        # write parquet file
+        with open(file_path, "wb") as f:
+            f.write(response.content)
+        print(f"file saved to {file_path} succesfully.")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()

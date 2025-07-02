c2e5646326e7287e2d09e6dcd4a8b1fa387e1711
jerome <jeromechua.jz@gmail.com>
2025-07-02 22:40:59 +0800

    added snowflake connector config

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from data_cleaning.clean import clean_data
 from data_loading.load import create_table, insert_data
 from pyspark.sql import SparkSession
 
+
 def main():
     file_path = extract_data()
     if not file_path:
@@ -12,6 +13,8 @@ def main():
     # create spark session
     spark = SparkSession.builder\
         .appName("nyc_taxi_etl")\
+        .config("spark.jars.packages",
+                "net.snowflake:snowflake-jdbc:3.13.22,net.snowflake:spark-snowflake_2.12:2.11.0-spark_3.3")\
         .getOrCreate()
     df = spark.read.parquet("data/yellow_tripdata_2024-01.parquet")
     df_cleaned = clean_data(df)
